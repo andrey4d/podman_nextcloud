508a645a7669ac4f2d3a04278045970005acf6f5
andrey <andrey4d.dev@gmail.com>
2021-10-10 15:55:04 +0700

    RENAME nextcloud.sh to nextcloud

--- clean.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-source nextcloud.properties
-
-${ENGINE} pod stop "${POD_NAME}"
-#${ENGINE} stop "${CONATINER_DB_NAME}"
-${ENGINE} pod rm "${POD_NAME}"
-${ENGINE} network rm "${NETWORK_NAME}"
